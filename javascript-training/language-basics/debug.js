53a37aada6b242ffe4c9e06da5be208d25a1c6f7
Ojas Kamble <iamojask96@gmail.com>
2026-04-09 22:57:22 +0530

    message

---
+++ javascript-training/language-basics/debug.js
@@ -0,0 +1,23 @@
+console.log('Executing line 1');
+console.log('Executing line 2');
+console.log('Executing line 3');
+sum(5,10);
+console.log('Executing line 4');
+console.log('Executing line 5');
+console.log('Executing line 6');
+console.log('Executing line 7');
+console.log('Executing line 8');
+console.log('Executing line 9');
+console.log('Executing line 10');
+
+//Logic to get sum of 2 nums
+function sum(a,b){
+    c=a-b ;
+    console.log(c);
+}
+
+//Debug the program
+//1.Adding the breakpoints.(The point where stop esecte the breakpoint)
+//2.Run the program in debug mode.
+//3.
+
---
+++ javascript-training/language-basics/hello-world.js
@@ -0,0 +1,5 @@
+console.log('Hello, World!');
+
+console.error('This is error');
+console.warn('This is warning');
+console.info('This is information');
